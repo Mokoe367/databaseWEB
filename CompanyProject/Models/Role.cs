9ccce15c0e9abae21e9a5dd8eac9ab4e4382d392
koifishnatalie <127334378+koifishnatalie@users.noreply.github.com>
2023-03-15 11:44:33 -0500

    Add files via upload

---
+++ CompanyProject/Models/Role.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyProject.Models
+{
+    public class Role
+    {
+        private CompanyContext context;
+        public int roleID { get; set; }
+        public string roleName { get; set; }
+
+
+    }
+}
