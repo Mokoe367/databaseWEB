913aba54f9330547fa9eb6f40271eab6501b2587
koifishnatalie <127334378+koifishnatalie@users.noreply.github.com>
2023-03-15 10:51:08 -0500

    Update Department.cs

--- CompanyProject/Models/Department.cs
+++ CompanyProject/Models/Department.cs
@@ -1,1 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
+namespace CompanyProject.Models
+{
+    public class Department
+    {
+        private CompanyContext context;
+        public int depID { get; set; }
+        public string location { get; set; }
+        public string depName { get; set; }
+        public string mgrID { get; set; }
+        public string projID { get; set; }
+        public string mgrSSN { get; set; }
+        
+
+    }
+}
