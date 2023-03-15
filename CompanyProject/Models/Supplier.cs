2d352045869015d2bedfbd06619914ad57015955
koifishnatalie <127334378+koifishnatalie@users.noreply.github.com>
2023-03-15 11:24:14 -0500

    Add files via upload

---
+++ CompanyProject/Models/Supplier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyProject.Models
+{
+    public class Supplier
+    {
+        private CompanyContext context;
+        public int supID { get; set; }
+        public string product { get; set; }
+        public string supName { get; set; }
+        public int roleID { get; set; }
+
+
+    }
+}
