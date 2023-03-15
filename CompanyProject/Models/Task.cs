7fab75a8edf9b835ed9dfb57fbc75bd54ae54422
koifishnatalie <127334378+koifishnatalie@users.noreply.github.com>
2023-03-15 11:27:56 -0500

    Add files via upload

---
+++ CompanyProject/Models/Task.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyProject.Models
+{
+    public class Task
+    {
+        private CompanyContext context;
+        public int taskID { get; set; }
+        public string taskName { get; set; }
+        public DateTime taskDueDate { get; set; }
+        public int cost { get; set; }
+        public int employeeID { get; set; }
+        public int projID { get; set; }
+
+
+    }
+}
