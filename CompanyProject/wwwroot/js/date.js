d11ae629ebdec8a4a4049e43ed9f5538392954d2
Mokoe367 <joshsevilla777@gmail.com>
2023-04-18 13:27:55 -0500

    dates and alerts and logins

---
+++ CompanyProject/wwwroot/js/date.js
@@ -0,0 +1,14 @@
+﻿let today = new Date();
+let todayDay = today.getDate();
+let todayMonth = today.getMonth() + 1;
+let todayYear = today.getFullYear() - 18;
+
+if (todayMonth < 10) {
+    todayMonth = "0" + todayMonth.toString();
+}
+if (todayDay < 10) {
+    todayDay = "0" + todayDay.toString();
+}
+
+let string = todayYear.toString() + "-" + todayMonth.toString() + "-" + todayDay.toString();
+document.getElementById("BirthDate").setAttribute('max', string);
