d7134234cff9c729d5b901a94f58733507cebaf8
abdajiro <abdulajirotutu@gmail.com>
2023-04-23 01:41:00 -0500

    team report stuff

---
+++ CompanyProject/Models/TeamTasks.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyProject.Models
+{
+    public class TeamTasks
+    {
+        public string Fname { get; set; }
+
+        public string Lname { get; set; }
+        public int taskID { get; set; }
+
+        public string taskName { get; set; }
+
+        public string documentation { get; set; }
+
+        public string projName { get; set; }
+
+        public int projID { get; set; }
+
+
+        public int employeeID { get; set; }
+    }
+}
